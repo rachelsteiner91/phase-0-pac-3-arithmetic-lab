65cdc6c71314a6a7ae7c071ac31b24dc56e3f330
Rachel Steiner <steinerr310@gmail.com>
2023-03-20 17:05:26 -0400

    arithmetic lab complete

---
+++ index.js
@@ -0,0 +1,26 @@
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+function add() {return "something"}
+function add(a,b) {return "something"}
+function add(a,b) {return a +b}
+function subtract() {return "something"}
+function subtract(a,b) {return "something"}
+function subtract(a,b) {return a - b}
+function multiply() {return "something"}
+function multiply(a,b) {return "something"}
+function multiply(a,b) {return a * b}
+function divide() {return "something"}
+function divide(a,b) {return "something"}
+function divide(a,b) {return a / b}
+function increment() {return "something"}
+function increment(n) {return "something"}
+function increment(n) {return ++n}
+function decrement() {return "something"}
+function decrement(n)  {return "something"}
+function decrement(n) {return --n}
+function makeInt(n) {return "something"}
+function makeInt(n) {return parseInt(n,10)}
+function preserveDecimal(n) {return "something"}
+function preserveDecimal(n) {return parseFloat(n,10)}
